b0e9b343ae39baddab22089d8647b9653ea2e167
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-10-08 11:55:26 +0530

    def pri(n):     if(n==1):         return 0     if(n==2):         return 1     for i in range(2,int(math.sqrt(n))+1):         if(n%i==0):             return 0     return 1 import math t=int(input()) while(t):     n=int(input())     p=n     s=n     while(n):         if(pri(n)):             x=n             break         n+=1     while(s):         if(pri(s)):             y=s             break         s-=1     if(x-p>=p-y):         print(y)     else:         print(x)     t-=1

---
+++ Nearest_Prime.py
@@ -0,0 +1,30 @@
+def pri(n):
+    if(n==1):
+        return 0
+    if(n==2):
+        return 1
+    for i in range(2,int(math.sqrt(n))+1):
+        if(n%i==0):
+            return 0
+    return 1
+import math
+t=int(input())
+while(t):
+    n=int(input())
+    p=n
+    s=n
+    while(n):
+        if(pri(n)):
+            x=n
+            break
+        n+=1
+    while(s):
+        if(pri(s)):
+            y=s
+            break
+        s-=1
+    if(x-p>=p-y):
+        print(y)
+    else:
+        print(x)
+    t-=1
